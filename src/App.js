df0b7051ebbc69c14847bc53576bfc3bd13c7c85
Tamilkumaran97 <tamilkumar7070@gmail.com>
2023-05-12 04:17:38 +0530

    first changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import "./App.css";
 import Exnav from "./Ligiotec Solution/Exnav";
-import Nav from "./Ligiotec Solution/Nav";
 
 function App() {
   return (
     <div className="App">
-     {/*  <Nav /> */}
-      <Exnav/>
+      <Exnav />
     </div>
   );
 }
