9096054d970eab2a90cf3b799a2175efd1c259a2
Tamil kumaran <132097362+Tamilkumaran97@users.noreply.github.com>
2023-05-29 12:22:45 +0530

    first changes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./App.css";
 
 import Exnav from "./Ligiotec Solution/Exnav";
 import { LocomotiveScrollProvider } from "react-locomotive-scroll";
 import "locomotive-scroll/dist/locomotive-scroll.css";
 import SecondPage from "./Ligiotec Solution/SecondPage";
+
+import Loding from "./Ligiotec Solution/Loding";
 function App() {
   const containerRef = useRef(null);
+
+  const [loading, setLoading] = useState(false);
+
+   useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 8000);
+  }, []);
   return (
     <div className="App">
       <LocomotiveScrollProvider
         options={{
           smooth: true,
           // ... all available Locomotive Scroll instance options
+          smartphone: {
+            smooth: true,
+          },
+          tablet: {
+            smooth: true,
+          },
         }}
         watch={
           [
@@ -24,9 +41,14 @@ function App() {
         containerRef={containerRef}
       >
         <main data-scroll-container ref={containerRef}>
-          <Exnav />
-          <SecondPage />
-          {/*  <ThirdPage /> */}
+          {loading ? (
+            <Loding />
+          ) : (
+            <div>
+              <Exnav />
+              <SecondPage />
+            </div>
+          )}
         </main>
       </LocomotiveScrollProvider>
     </div>
--- src/Ligiotec Solution/SecondPage.js
+++ src/Ligiotec Solution/SecondPage.js
@@ -8,7 +8,8 @@ import smoke from "./assets/fog.png";
 import building from "./assets/building.png";
 import plane from "./assets/airplane.png";
 import bgst from "./assets/veiw.avif";
-
+import sun from "./assets/sun.png";
+import moon from "./assets/moon.png";
 import cld from "./assets/cloud1.png";
 import web from "./assets/web.png";
 import phone from "./assets/phone.png";
@@ -22,52 +23,74 @@ import github from "./assets/github-svgrepo-com.svg";
 import twitter from "./assets/twitter-rounded-svgrepo-com.svg";
 import face from "./assets/facebook-round-svgrepo-com.svg";
 import tele from "./assets/telegram-fill-svgrepo-com.svg";
+import start from "./assets/start.png";
 
 const SecondPage = () => {
   return (
-    <section id="section ">
-      {/* first page */}
+    <section id="section">
+      {/* -----------------first page--------------------- */}
       <div
         id="fixed"
-        className="  relative  w-[100%] h-[100vh] overflow-hidden"
+        className="  relative w-[100%] h-[198px] lg:w-[100%] lg:h-[100vh] overflow-hidden"
       >
+        {/**----------------moon-------------- */}
+        <div
+          data-scroll
+          data-scroll-direction="vertical"
+          data-scroll-speed="-13.8"
+          className="absolute lg:left-[800px] left-[190px] opacity-90 z-10 "
+        >
+          <img className="bg-blend-screen" src={moon} alt="" />
+        </div>
         {/*   flight one */}
 
         <motion.div
           data-scroll
           data-scroll-speed="24"
           data-scroll-direction="horizontal"
-          className=" mt-0 lg:mt-40 left-[900px] absolute z-20  "
+          className=" mt-40 left-[900px] absolute z-20  "
         >
-          <img className="w-52 lg:w-[900px]" src={plane} alt="" />
+          <img className="lg:visible invisible w-[900px]" src={plane} alt="" />
+        </motion.div>
+        {/** flight one-copy */}
+        <motion.div
+          initial={{ x: 300 }}
+          whileInView={{ x: -200 }}
+          transition={{ duration: 6, repeat: Infinity }}
+          className="   absolute z-20  "
+        >
+          <img className="visible lg:invisible w-[200px]" src={plane} alt="" />
         </motion.div>
         {/**building */}
         <motion.div>
           <img
             data-scroll
-            data-scroll-speed="-1"
-            className="lg:visible invisible lg:absolute lg:z-30  lg:w-full "
+            data-scroll-speed=""
+            className=" absolute z-30  lg:w-full "
             src={building}
             alt=""
           />
         </motion.div>
         {/*  cloud img */}
-        <motion.div className=" lg:w-full">
+        <motion.div className="w-full lg:w-full">
           <img
             data-scroll
-            data-scroll-speed="-1"
-            className="lg:visible invisible lg:absolute w-[2000px] lg:w-full"
+            data-scroll-speed=""
+            className=" absolute lg:w-full "
             src={smoke}
             alt=""
           />
+          {/*    <div className="bg-red-500 -mt-[500px] w-full h-[100vh] lg:invisible absolute">
+
+          </div> */}
         </motion.div>
         {/* man image */}
 
-        <div className="lg:visible invisible absolute top-[40px] lg:top-[190px] z-20">
+        <div className=" absolute top-[90px]  lg:top-[190px] z-20">
           <img
             data-scroll
             data-scroll-speed="-1"
-            className=" w-[300px] lg:w-[900px]  "
+            className="w-[200px]  lg:w-[900px]  "
             src={man}
             alt=""
           />
@@ -87,45 +110,78 @@ const SecondPage = () => {
             data-scroll
             data-scroll-sticky
             data-scroll-target="#fixed"
-            className="text-4xl lg:text-7xl  lg:text-white drop-shadow-[0_35px_35px_rgb(255,255,255)] z-10 font-bold lg:w-1/2 uppercase -mt-[600px]  lg:mt-60"
+            className="text:xl lg:text-7xl text-white drop-shadow-[0_35px_35px_rgb(255,255,255)] pl-2 z-10 font-bold lg:w-1/2 uppercase  mt-24 lg:mt-60"
           >
             Business consultancy
           </div>
+          {/**Business content  */}
           <div
             data-scroll
             data-scroll-speed="4"
-            className="font-bold lg:text-white  z-10  lg:w-1/2 lg:mr-52  lg:text-2xl  mt-40"
+            className="font-bold lg:visible invisible  text-white z-40  lg:z-10 text-[12px]  w-1/2   -ml-4     lg:ml-16  lg:text-2xl mt-10 lg:mt-40"
           >
             <ul className="">
-              <li> Start A Business</li>
-              <li>Accounting & Financing</li>
-              <li>Secretarial Compliance</li>
-              <li>Taxation</li>
-              <li> Advisory</li>
-              <li> Licence and Business permits</li>
+              <li className="pt-2"> Start A Business</li>
+              <li className="pt-2">Accounting & Financing</li>
+              <li className="pt-2">Secretarial Compliance</li>
+              <li className="pt-2">Taxation</li>
+              <li className="pt-2"> Advisory</li>
+              <li className="pt-2"> Licence and Business permits</li>
+            </ul>
+          </div>
+          {/**Business content copy */}
+          <div className="font-bold lg:invisible visible  text-white z-40   text-[12px]  w-1/2  -ml-40     mt-5 ">
+            <ul className="">
+              <li className="pt-2"> Start A Business</li>
+              <li className="pt-2">Accounting & Financing</li>
+              <li className="pt-2">Secretarial Compliance</li>
+              <li className="pt-2">Taxation</li>
+              <li className="pt-2"> Advisory</li>
+              <li className="pt-2"> Licence and Business permits</li>
             </ul>
           </div>
         </div>
       </div>
-      <div className=" relative  w-[100%] h-[100vh] overflow-hidden flex">
-        <img className="absolute w-[100%] blur-sm" src={bgst} alt="" />
-        {/*  <img className="absolute w-full blur-sm z-1 " src={street} alt="" /> */}
+      {/*  -----------------second page-------------------- */}
+      <div className=" relative h-[199px]  lg:w-[100%] lg:h-[100vh] overflow-hidden flex">
+        {/**sun */}
+        <div
+          data-scroll
+          data-scroll-speed="-7.5"
+          data-scroll-direction="vertical"
+          className="absolute z-10 opacity-90 left-[190px] lg:left-[840px]"
+        >
+          <img className="w-[10rem] lg:w-[29rem]" src={sun} alt="" />
+        </div>
+        <img className="absolute lg:w-[100%] blur-sm" src={bgst} alt="" />
+        {/*------------ flight-image -----------   */}
         <motion.div
           initial={{ x: 900 }}
           whileInView={{ x: -100 }}
           transition={{ duration: 4, delay: 0.3 }}
-          className="absolute w-[500px] mt-96"
+          className="lg:visible invisible  absolute  w-[500px] mt-24 lg:mt-96"
         >
           <div className="w-[2000px] h-2 absolute top-[150px] left-[350px] flex justify-end rounded-l-full blur-sm bg-white/50"></div>
 
-          <img className="ml-[150px] w-[350px]" src={Img1} alt="" />
+          <img className="ml-[150px]  lg:w-[350px]" src={Img1} alt="" />
         </motion.div>
+        {/*------------ flight-image copy -----------   */}
+        <motion.div
+          initial={{ x: 300 }}
+          whileInView={{ x: -200 }}
+          transition={{ duration: 4, delay: 0.3 }}
+          className="lg:invisible visible  absolute  w-[500px] mt-24 lg:mt-96"
+        >
+          <div className="w-[2000px] h-2 absolute top-[70px] left-[250px] flex justify-end rounded-l-full blur-sm bg-white/50"></div>
 
+          <img className="ml-[150px]  w-[150px]" src={Img1} alt="" />
+        </motion.div>
+        {/**-----------super market content------------- */}
         <h1
           data-scroll
           data-scroll-speed="-5"
           data-scroll-direction="vertical"
-          className="font-bold mt-16 text-7xl w-1/2 uppercase text-white"
+          className="font-bold z-20 ml-8 lg:ml-0  lg:mt-16 text-xl lg:text-7xl lg:w-1/2  lg:pl-2 uppercase text-white lg:text-white"
         >
           Supermarket consultancy
         </h1>
@@ -133,7 +189,7 @@ const SecondPage = () => {
           initial={{ x: 600 }}
           whileInView={{ x: 0 }}
           transition={{ duration: 2, delay: 0.9 }}
-          className="font-bold text-white  w-1/2 mr-14 z-10 text-2xl  mt-40"
+          className="lg:visible invisible font-bold text-white  w-1/2 -ml-16 z-10 text-2xl   mt-40"
         >
           <li className="mt-2"> New Store Startup</li>
           <li className="mt-2"> Merchandising</li>
@@ -143,18 +199,35 @@ const SecondPage = () => {
           <li className="mt-2"> Stock Auditing</li>
           <li className="mt-2">Infrastructure Costing</li>
         </motion.ul>
+        {/**-------------Submenu copy----------------- */}
+        <motion.ul
+          initial={{ x: 200 }}
+          whileInView={{ x: 0 }}
+          transition={{ duration: 3 }}
+          className="font-bold visible lg:invisible text-white -ml-[330px] mt-10 text-center  z-10 text-sm   "
+        >
+          <li className=""> New Store Startup</li>
+          <li className=""> Merchandising</li>
+          <li className="">Shop Development</li>
+          <li className="">Marketing strategy</li>
+          <li className=""> Product Procurement</li>
+          <li className=""> Stock Auditing</li>
+          <li className="">Infrastructure Costing</li>
+        </motion.ul>
         <motion.div
           initial={{ opacity: 0 }}
           whileInView={{ opacity: 1 }}
           transition={{ duration: 2, delay: 1.5 }}
-          className="top-[200px] left-[980px]  absolute"
+          className="lg:top-[200px] left-[220px] z-20 top-[70px] lg:left-[980px]  absolute"
         >
-          <img src={Img2} alt="" />
+          <img className="w-[8rem]  lg:w-[23rem]" src={Img2} alt="" />
         </motion.div>
       </div>
+
+      {/**---------------------Third page-------------------------- */}
       <div
         id="flow"
-        className="  relative   w-[100%] h-[300vh]  overflow-hidden "
+        className="  relative h-[350vh]  lg:w-[100%]  lg:h-[300vh]  overflow-hidden "
       >
         <div
           id="stick"
@@ -167,10 +240,10 @@ const SecondPage = () => {
           <div className="absolute">
             <div className=" -mt-40 -ml-4 ">
               <div
-                className="p-6 bg-[#ffc914] lg:flex h-screen 
+                className="p-6 bg-[#ffc914] lg:flex lg:h-screen 
        "
               >
-                <motion.div className="mt-48 lg:mt-44 w-[500px]">
+                <motion.div className="mt-40 lg:mt-44 w-[500px]">
                   <h1 className="squad font-bold text-5xl ">
                     Ligiotec Solution
                   </h1>
@@ -270,7 +343,7 @@ const SecondPage = () => {
                 </motion.div>
               </div>
             </div>
-            <div className=" bg-[#ffc914] absolute gap-[55px] p-10 flex -mt-[100px]  border-t border-black  ">
+            <div className=" bg-[#ffc914] absolute gap-[55px] p-10 flex  lg:-mt-[100px] lg:visible invisible  border-t border-black  ">
               <div className="text-[13px] lg:text-sm  w-auto">
                 <h1 className="font-bold   lg:text-xl">Company</h1>
                 <ul>
@@ -346,12 +419,21 @@ const SecondPage = () => {
           </div>
         </div>
 
-        <div className="w-full bg-[#37C3DEff] rounded-b-[100px] absolute  h-[200vh]  ">
+        <div className="w-full bg-[#37C3DEff] lg:rounded-b-[100px] absolute  h-[150vh]  lg:h-[200vh]  ">
+          <div>
+            <img
+              data-scroll
+              data-scroll-speed="4"
+              className=" w-full h-[200vh] opacity-20 absolute"
+              src={start}
+              alt=""
+            />
+          </div>
           <img
             data-scroll
             data-scroll-direction="horizontal"
             data-scroll-speed="-2"
-            className="w-[500px] z-20 opacity-90 absolute"
+            className="w-[200px] lg:w-[500px] z-20 opacity-90 absolute"
             src={cloud}
             alt=""
           />
@@ -360,26 +442,30 @@ const SecondPage = () => {
               data-scroll
               data-scroll-direction="vertical"
               data-scroll-speed="-20"
-              className="absolute w-[300px] ml-20  "
+              className=" absolute w-[300px] ml-20  "
               src={top}
               alt=""
             />
           </div>
-          <div id="one" className=" w-full h-[100vh]  ml-24">
+          <div id="one" className=" w-full h-[100vh]  lg:ml-24">
             <div
               data-scroll
               data-scroll-speed="20"
               data-scroll-direction="horizontal"
-              className="text-8xl mt-20 uppercase p-4 font-bold flex justify-center"
+              className="text-6xl text-center  lg:text-8xl mt-20 uppercase lg:p-4 font-bold flex justify-center"
             >
               Cloud Web Hosting
             </div>
             <div data-scroll data-scroll-speed="4" className="absolute mt-24">
-              <img className="w-[800px] opacity-75 ml-40 " src={cloud} alt="" />
+              <img
+                className="w-[800px] opacity-75 lg:ml-40 "
+                src={cloud}
+                alt=""
+              />
             </div>
             <div
               id="two"
-              className="absolute left-[900px] font-bold text-2xl mt-20"
+              className="absolute   text-center  lg:left-[900px] font-bold text-2xl mt-20"
             >
               <motion.ul
                 initial={{ x: 200 }}
@@ -401,7 +487,7 @@ const SecondPage = () => {
                 data-scroll
                 data-scroll-sticky="center"
                 data-scroll-target="#two"
-                className="uppercase text-6xl py-20"
+                className="uppercase text-4xl ml-4 lg:text-6xl py-20"
               >
                 App Development
               </div>
@@ -409,10 +495,10 @@ const SecondPage = () => {
                 <li> Website Development</li>
                 <li> CRM Development</li>
                 <li>E-Commerce Development</li>
-                <li>Mobile Application Development</li>
+                <li className="px-3">Mobile Application Development</li>
                 <li> SEO</li>
                 <li> Automation</li>
-                <li>REST API Development & Integration</li>
+                <li>REST API Development </li>
               </ul>
             </div>
 
@@ -422,7 +508,7 @@ const SecondPage = () => {
                   data-scroll
                   data-scroll-sticky
                   data-scroll-target="#two"
-                  className="relative "
+                  className="lg:visible invisible relative "
                   src={web}
                   alt=""
                 />
@@ -433,17 +519,17 @@ const SecondPage = () => {
                   data-scroll
                   data-scroll-speed="-4"
                   data-scroll-direction="horizontal"
-                  className="absolute -mt-[300px] ml-24"
+                  className="  absolute -mt-[250px] lg:-mt-[300px] ml-72 lg:ml-24"
                   src={cld}
                   alt=""
                 />
               </div>
             </div>
           </div>
-          <div className="bg-[#37C3DEff] w-[900px] relative ">
+          <div className="bg-[#37C3DEff] rounded-b-[20px] w-[900px] relative ">
             {/*  <img src={top} alt="" /> */}
             <img
-              className="w-[500px] relative mt-52 ml-20 "
+              className="w-[400px] lg:w-[500px] relative mt-52 ml-20 "
               src={phone}
               alt=""
             />
@@ -451,7 +537,7 @@ const SecondPage = () => {
               data-scroll
               data-scroll-speed="2"
               data-scroll-direction="vertical"
-              className="absolute ml-10 -mt-[22rem]"
+              className="w-[500px] absolute ml-10 -mt-[22rem]"
               src={up}
               alt=""
             />
